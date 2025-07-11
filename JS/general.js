ef19b52a5b893e04638d02435fdafdb7294c79d8
AguelDawma <thapelosekhonyana37@gmail.com>
2025-07-11 16:32:09 +0200

    added nav-logged-in display none to CSS general

---
+++ JS/general.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded' , function(){
+    const menuIcon = document.querySelector('.menuBar');
+    console.log(menuIcon);
+    const mainNav = document.querySelector('.menu-nav');
+
+    menuIcon.addEventListener('click' , function(){
+        menuIcon.classList.toggle('active');
+        mainNav.classList.toggle('active');
+    })
+})
+
+console.log("I am in");
