6e6455650f27f2ed0f99894de49e48d99d0fec00
AguelDawma <thapelosekhonyana37@gmail.com>
2025-07-20 17:54:37 +0200

    added search nput and its transitions to appear on screen

--- JS/general.js
+++ JS/general.js
@@ -2,9 +2,19 @@ document.addEventListener('DOMContentLoaded' , function(){
     const menuIcon = document.querySelector('.menuBar');
     const mainNav = document.querySelector('.menu-nav');
 
+    const searchIcon = document.querySelector('.search');
+    const searchContainer = document.querySelector('.search-container');
+    const searchInput = document.querySelector('.search-input');
+
     menuIcon.addEventListener('click' , function(){
         menuIcon.classList.toggle('active');
         mainNav.classList.toggle('active');
+    })
+
+    searchIcon.addEventListener('click' , function(){
+        searchContainer.classList.toggle('active');
+        searchIcon.classList.toggle('active');
+        searchInput.classList.toggle('active');
     })
 })
 
