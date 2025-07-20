71fac2c6850a67b34570d20ffb204f4be0b8a41a
AguelDawma <thapelosekhonyana37@gmail.com>
2025-07-20 11:26:57 +0200

    added lorem ipsum lines, merged the menu lines and added icons in the navigation menu

--- JS/general.js
+++ JS/general.js
@@ -1,12 +1,10 @@
 document.addEventListener('DOMContentLoaded' , function(){
     const menuIcon = document.querySelector('.menuBar');
-    console.log(menuIcon);
     const mainNav = document.querySelector('.menu-nav');
 
     menuIcon.addEventListener('click' , function(){
-        this.classList.toggle('active');
+        menuIcon.classList.toggle('active');
         mainNav.classList.toggle('active');
     })
 })
 
-console.log("I am in");
