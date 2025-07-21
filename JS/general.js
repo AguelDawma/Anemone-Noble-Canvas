0cd89312bbd4db467cea98aea952f161c31dbab5
AguelDawma <thapelosekhonyana37@gmail.com>
2025-07-21 17:11:20 +0200

    edited unclickability of main content and scrolling inability

--- JS/general.js
+++ JS/general.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded' , function(){
+    const body = document.body
     const menuIcon = document.querySelector('.menuBar');
     const mainNav = document.querySelector('.menu-nav');
 
@@ -9,12 +10,14 @@ document.addEventListener('DOMContentLoaded' , function(){
     menuIcon.addEventListener('click' , function(){
         menuIcon.classList.toggle('active');
         mainNav.classList.toggle('active');
+        body.classList.toggle('nav-active');
     })
 
     searchIcon.addEventListener('click' , function(){
         searchContainer.classList.toggle('active');
         searchIcon.classList.toggle('active');
         searchInput.classList.toggle('active');
+        body.classList.toggle('search-active');
     })
 })
 
